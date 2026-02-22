d14d24aa54f67bfa7797f2454d52e52cea17ce6c
Apollo-CPP <DylanZhangRBLX2010@gmail.com>
2026-02-22 11:15:27 -0800

    Learning Numpy from the official Documentation

--- Python_Projects/Python_Learning/Library_Practice/Numpy_Practice/Numpy_Foundations_Practice.py
+++ Python_Projects/Python_Learning/Library_Practice/Numpy_Practice/Numpy_Foundations_Practice.py
@@ -256,3 +256,41 @@ New_Array = More_Arrays[A_Filtered_List]
 print(A_Filtered_List)
 print(New_Array)
 print("--------------------")
+
+# ========== Numpy Official Documentation for Beginners ==========
+
+# You can add dimensions by using np.newaxis() and np.expand_dims
+Lot_of_Numbers = np.arange(1, (20) + 1)
+print(Lot_of_Numbers)
+
+# np.newaxis is equivalent to None
+# Creates a VIEW of the original array and slices the original array's elements to keep them
+# Yes, order DOES matter. If you put np.newaxis at the front and slice at the end, it will create more columns
+Expanded_Columns = Lot_of_Numbers[np.newaxis, :]
+print(Expanded_Columns)
+
+# If you want to add more rows then slice at the front and np.newaxis at the end
+Expanded_Rows = Lot_of_Numbers[:,  np.newaxis]
+print(Expanded_Rows)
+
+# Alternative Way: np.expand_dims()
+Alternative_Column_Expansion = np.expand_dims(Lot_of_Numbers, axis=0)
+print(Alternative_Column_Expansion)
+
+Alternative_Row_Expansion = np.expand_dims(Lot_of_Numbers, axis=1)
+print(Alternative_Row_Expansion)
+
+# Filtering arrays
+Even_Numbers_Array = Lot_of_Numbers[Lot_of_Numbers%2 == 0]
+print(Even_Numbers_Array)
+
+# Find even numbers that are greater than 10
+Conditional_Array = Lot_of_Numbers[(Lot_of_Numbers%2 == 0) & (Lot_of_Numbers > 10)] # Use the & for and and | for or in conditional sorting
+# Also use (condition) to separate conditions (NEEDED)
+print(Conditional_Array)
+
+# To find all values that are NOT zero, you can use the np.nonzero() function
+print(np.nonzero(Even_Numbers_Array))
+
+More_Random_Numbers_Array = np.array([[56, 1, 0], [3, 0, 0], [1, 6, 3], [82, 5, 1]])
+print(np.nonzero(More_Random_Numbers_Array)) # Since it is a 2D array, the first array will be the rows and the second array will be the columns
