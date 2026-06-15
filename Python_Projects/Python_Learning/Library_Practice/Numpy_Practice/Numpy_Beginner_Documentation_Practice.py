308dae862117b33def3a8d2de7d0b16a2368e1a3
Apollo-CPP <DylanZhangRBLX2010@gmail.com>
2026-06-15 15:48:59 -0700

    Resetted my PC for a fresh restart lol since im going to UOP summer institute tomorrow

---
+++ Python_Projects/Python_Learning/Library_Practice/Numpy_Practice/Numpy_Beginner_Documentation_Practice.py
@@ -0,0 +1,228 @@
+# ========== Numpy Official Documentation for Beginners ==========
+
+import numpy as np
+
+# To create an array use the np.array() function, which takes in a list
+My_First_Numpy_Array = np.array([2, 78, 7, 6, 4, 13, 37, 53])
+print(My_First_Numpy_Array)
+
+# The type of a Numpy array is an ndarray
+print(type(My_First_Numpy_Array))
+
+# Some fundamental properties of a Numpy array are ndim (Number of dimensions), shape, size, and dtype (data type)
+print(My_First_Numpy_Array.ndim) # This is a 1D array so it has 1 dimensional so it will print 1
+
+# The shape of an array is basically the amount of depth, rows, and columns of an array (if it is more than one dimension)
+print(My_First_Numpy_Array.shape) # Returns a tuple (why there's a "," after) of the shape and there are 8 elements in the 1D array
+
+# Size is basically the amount of elements in the array
+print(My_First_Numpy_Array.size)
+
+# dtype or Data type is literally self-explanatory, data type of the array
+print(My_First_Numpy_Array.dtype)
+
+# There are also many ways to create a basic array other than np.array()
+# Examples: np.zeros(), np.ones(), np.empty(), np.arange(), and np.linspace()
+
+Zeros_Array = np.zeros(10) # This crates a NumPy Array filled with 10 floats (by default) that are literally 0's
+print(Zeros_Array)
+
+Ones_Array = np.ones(5) # Just like np.zeros() except it's all 1's
+print(Ones_Array)
+
+Idk_if_this_is_empty = np.empty(10) # Makes an "empty" array (not really) but it's purpose is to make uninitialized array if you're going to immediately overwrite its elements later
+# I could've sworn that it fills it with random numbers but now it's 0's??
+print(Idk_if_this_is_empty)
+
+# You could also specify a range of numbers to be put in a NumPy Array
+My_Range_Array = np.arange((10) + 1) # It's just like range(11) - Numbers from 0 to 10
+print(My_Range_Array)
+
+# You can even specify a linearly spaced array using linspace()
+Spaced_Array = np.linspace(2, 8, num=4) # Basically just means a total of four numbers that start from 2 and stop at 8 but are all equally stepped or spaced
+# Also it's a float by default
+print(Spaced_Array)
+
+# Sorting and concatenating arrays
+Not_Sorted_Array = np.array([567, 24, 35, 21, 647, 89, 34, 58, 78, 234])
+
+# Use the np.sort() function to sort any array
+print(np.sort(Not_Sorted_Array))
+
+# You can join or combine two arrays using the np.concatenate() function
+Wow_Combine_Me = np.array([[1, 2, 3], [4, 5, 6]])
+Me_Too_Pls = np.array([[7, 8, 9], [10, 11, 12]])
+
+print(np.concatenate((Wow_Combine_Me, Me_Too_Pls)))
+
+# You can even add an axis on how to concatenate arrays
+print(np.concatenate((Wow_Combine_Me, Me_Too_Pls), axis=0)) # Combines them vertically
+print(np.concatenate((Wow_Combine_Me, Me_Too_Pls), axis=1)) # Combines them horizontally
+
+# Creating array using existing data (vstack, hstack, hsplit, vsplit, view, and copy)
+
+Yeet_2D_Array = np.array([[1, 2, 3], [4, 5, 6]])
+Ok_2D_Array = np.array([[7, 8, 9], [10, 11, 12]])
+
+# To stack an array vertically use the np.vstack() function
+print(np.vstack((Yeet_2D_Array, Ok_2D_Array)))
+
+# To stack an array horizontally use the np.vstack() function
+print(np.hstack((Yeet_2D_Array, Ok_2D_Array)))
+
+Random_Arange = np.arange(1, (50) + 1).reshape(5, 10)
+
+# You can use either np.hsplit() or np.vsplit() to split an array into smaller arrays
+print(np.hsplit(Random_Arange, 5)) # Splits the array horizontally
+print(np.vsplit(Random_Arange, 5)) # Splits the array veritcally (Only works on 2D arrays)
+
+# Copy and Views
+
+Random_Numbers_Again = np.array([6, 3, 9, 5, 1, 6, 9, 54, 23, 7, 1])
+
+My_Copy = Random_Numbers_Again.copy() # This is a copy, a complete separate array from the original array
+My_View = Random_Numbers_Again.view() # This is a view, basically an alias to the original array
+
+# If you modify a copy, it won't affect the original array but if you modify a view then it will change the original array
+My_Copy[0] = 4556
+print(Random_Numbers_Again) # No effect
+
+My_View[0] = 4576765
+print(Random_Numbers_Again) # Changes the value 6 at index 0 to 4576765
+
+# Basic arithmatic operations
+Numbers_Array = np.array([5, 2, 8])
+More_Numbers_Array = np.array([7, 1, 6])
+
+print(Numbers_Array + More_Numbers_Array) # Just adds the arrays across, same with all other basic math operations
+print(Numbers_Array - More_Numbers_Array)
+print(Numbers_Array * More_Numbers_Array)
+print(Numbers_Array / More_Numbers_Array)
+
+# There are also some built in operations as well
+print(np.sum(Numbers_Array)) # Finds the sum of the array
+
+Combined_Numbers_Array = np.vstack((Numbers_Array, More_Numbers_Array))
+print(np.sum(Combined_Numbers_Array, axis=0)) # Can even find the sums vertically
+
+# Broadcasting in Numpy
+# Numpy has a lot of built in mathematical functions such as mean(), prod(), min(), max(), etc.
+
+print(np.mean(Combined_Numbers_Array)) # Finds the average of the array
+print(np.prod(Combined_Numbers_Array)) # Multiples every number together to get the product
+print(np.min(Combined_Numbers_Array)) # Finds the lowest number
+print(np.max(Combined_Numbers_Array)) # Finds the highest number
+
+# You can add dimensions by using np.newaxis() and np.expand_dims
+Lot_of_Numbers = np.arange(1, (20) + 1)
+print(Lot_of_Numbers)
+
+# np.newaxis is equivalent to None
+# Creates a VIEW of the original array and slices the original array's elements to keep them
+# Yes, order DOES matter. If you put np.newaxis at the front and slice at the end, it will create more columns
+Expanded_Columns = Lot_of_Numbers[np.newaxis, :]
+print(Expanded_Columns)
+
+# If you want to add more rows then slice at the front and np.newaxis at the end
+Expanded_Rows = Lot_of_Numbers[:, np.newaxis]
+print(Expanded_Rows)
+
+# Alternative Way: np.expand_dims()
+Alternative_Column_Expansion = np.expand_dims(Lot_of_Numbers, axis=0)
+print(Alternative_Column_Expansion)
+
+Alternative_Row_Expansion = np.expand_dims(Lot_of_Numbers, axis=1)
+print(Alternative_Row_Expansion)
+
+# Filtering arrays
+Even_Numbers_Array = Lot_of_Numbers[Lot_of_Numbers%2 == 0]
+print(Even_Numbers_Array)
+
+# Find even numbers that are greater than 10
+Conditional_Array = Lot_of_Numbers[(Lot_of_Numbers%2 == 0) & (Lot_of_Numbers > 10)] # Use the & for and and | for or in conditional sorting
+# Also use (condition) to separate conditions (NEEDED)
+print(Conditional_Array)
+
+# To find all values that are NOT zero, you can use the np.nonzero() function
+print(np.nonzero(Even_Numbers_Array))
+
+More_Random_Numbers_Array = np.array([[56, 1, 0], [3, 0, 0], [1, 6, 3], [82, 5, 1]])
+print(np.nonzero(More_Random_Numbers_Array)) # Since it is a 2D array, the first array will be the rows and the second array will be the columns
+
+# Generating random numbers in an array
+
+My_RNG = np.random.default_rng() # Creates a generator based off of the PCG64 generator
+print(My_RNG.random(10)) # Generates random floats between 0.0 and 1.0
+print("--------------------")
+My_RNG_Array = My_RNG.integers((10) + 1, size=(4, 4)) # Generates whole numbers from 0 to 10 (End is exclusive and starting point is 0 by DEFAULT) in a 2D 4x4 array
+print(My_RNG_Array)
+print("--------------------")
+
+# You can use np.unique() to find only unique numbers in the array
+print(np.unique(My_RNG_Array)) # Find unique values!
+print("--------------------")
+
+Unique_Values, Value_Indexes = np.unique(My_RNG_Array, return_index=True) # You can even return their indexes as well, just make sure to put two variables since it returns two things
+print(Unique_Values) # Print the unique values
+print("--------------------")
+print(Value_Indexes) # Print their indexes
+print("--------------------")
+
+Value_Counts = np.unique(My_RNG_Array, return_counts=True)
+print(Value_Counts) # Returns the amount of occurences a value has
+print("--------------------")
+
+# Transposing and reshaping arrays
+
+Transposing_Array = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+# Reshapes to 5 rows and 2 columns
+print(Transposing_Array.reshape(5, 2)) # Reshaping basically changes the dimensions of the array, it needs to multiply up to the amount of elements in the array
+
+# Rearranges the array's axes or shape. 5 rows and 2 columns becomes 2 rows and 5 columns
+print(Transposing_Array.reshape(5, 2).transpose())
+print(Transposing_Array.reshape(5, 2).T) # Numpy also provides the attribute or property T for a numpy array, which basically shows the transposed version of the array
+
+# Reversing arrays
+
+Reverse_Me_Array = np.arange(1, (10) + 1)
+Two_Dimensional_Reverse_Me_Array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# We can use the np.flip() function ro reverse Numpy arrays
+print(np.flip(Reverse_Me_Array)) # Reverses all elements
+
+# Reverses the order of the lists and the elements in those lists
+print(np.flip(Two_Dimensional_Reverse_Me_Array))
+
+# Axis=0 only reverses the order of the lists (vertical)
+print(np.flip(Two_Dimensional_Reverse_Me_Array, axis=0))
+
+# Axis=1 only reverses the elements inside of the lists of the NumPy Array
+print(np.flip(Two_Dimensional_Reverse_Me_Array, axis=1))
+
+# You can reverse a row
+print(np.flip(Two_Dimensional_Reverse_Me_Array[1])) # Reverses the second row
+
+# You can reverse columns (Example: Elements at column 1)
+print(np.flip(Two_Dimensional_Reverse_Me_Array[:, 1]))
+
+# "Flattening" arrays (Converting arrays back to 1 dimensional arrays)
+
+Random_2D_Array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Use the flatten() function to convert an array back to 1D
+print(Random_2D_Array.flatten())
+
+# You can also use the ravel() function to do this as well
+print(Random_2D_Array.ravel())
+
+# The difference between flatten() and ravel() is that flatten creates a copy of the array but ravel() creates a view of the array
+
+# You can use matplotlib to work with Numpy to graph
+import matplotlib.pyplot as plt
+
+Another_RNG = np.random.default_rng() # Make an RNG instance
+Even_More_Random_Numbers = Another_RNG.integers(1, (100) + 1, size=(100)) # Generate an array containing INTEGERS (not floats) with 100 random numbers ranging from 1 - 100
+print(Even_More_Random_Numbers)
+
+plt.plot(Even_More_Random_Numbers) # Plot the graph
+plt.show() # Show the graph
